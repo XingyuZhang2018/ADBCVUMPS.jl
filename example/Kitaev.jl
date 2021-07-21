f168d680a75ad4ff8f792908006e5955d9daf784
XingyuZhang2018 <zhangxingyu@iphy.ac.cn>
2021-07-21 21:20:00 +0800

    up from down

--- example/Kitaev.jl
+++ example/Kitaev.jl
@@ -10,7 +10,7 @@ CUDA.allowscalar(false)
 
 Random.seed!(100)
 model = Kitaev(-1.0,-1.0,-1.0)
-bcipeps, key = init_ipeps(model; atype = Array, D=2, χ=20, tol=1e-15, maxiter=10)
+bcipeps, key = init_ipeps(model; atype = CuArray, D=4, χ=20, tol=1e-15, maxiter=10)
 res = optimiseipeps(bcipeps, key; f_tol = 1e-10, opiter = 1000, verbose = true)
 e = minimum(res)
 @show e
