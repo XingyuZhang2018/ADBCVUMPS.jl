9d45997d85c61bc3280c4a8d23462db72b09e808
XingyuZhang2018 <zhangxingyu@iphy.ac.cn>
2021-08-26 20:37:37 +0800

    mag-degree

--- plot/mag-degree.jl
+++ plot/mag-degree.jl
@@ -1,13 +1,15 @@
 using ADBCVUMPS
 using ADBCVUMPS:σx,σy,σz,buildbcipeps
 using BCVUMPS
+using CUDA
+using FileIO
 using LinearAlgebra: norm
 using OMEinsum
 using Plots
 using Random
 
 function magnetisation(degree, folder, D, χ, tol, maxiter, miniter)
-    bulk, key = init_ipeps(Kitaev_Heisenberg(degree); folder = folder, atype = Array, D=D, χ=χ, tol=tol, maxiter=maxiter, miniter=miniter, verbose = true)
+    bulk, key = init_ipeps(Kitaev_Heisenberg(degree); folder = folder, atype = CuArray, D=D, χ=χ, tol=tol, maxiter=maxiter, miniter=miniter, verbose = true)
     folder, model, atype, D, χ, tol, maxiter, miniter = key
     Ni, Nj = 2, 2
     bulk = buildbcipeps(bulk,Ni,Nj)
@@ -16,19 +18,23 @@ function magnetisation(degree, folder, D, χ, tol, maxiter, miniter)
     ap = reshape(ap, Ni, Nj)
     a = [ein"ijklaa -> ijkl"(ap[i]) for i = 1:Ni*Nj]
     a = reshape(a, Ni, Nj)
-
-    env = obs_bcenv(model, a; atype = atype, D = D^2, χ = χ, tol = tol, maxiter = maxiter, miniter = miniter, verbose = false, savefile = false, folder = folder)
-
-    M, ALu, Cu, _, ALd, Cd, _, FL, FR = env
-    ap /= norm(ap)
+    
+    chkp_file_obs = folder*"$(model)_$(atype)/obs_D$(D^2)_chi$(χ).jld2"
+    FL, FR = load(chkp_file_obs)["env"]
+    chkp_file_up = folder*"$(model)_$(atype)/up_D$(D^2)_chi$(χ).jld2"                     
+    rtup = SquareBCVUMPSRuntime(a, chkp_file_up, χ; verbose = false)   
+    ALu,ARu,Cu = rtup.AL, rtup.AR, rtup.C
+    chkp_file_down = folder*"$(model)_$(atype)/down_D$(D^2)_chi$(χ).jld2"                              
+    rtdown = SquareBCVUMPSRuntime(a, chkp_file_down, χ; verbose = false)   
+    ALd,ARd,Cd = rtdown.AL,rtdown.AR,rtdown.C
 
     M = zeros(2,2)
     for j = 1:Nj, i = 1:Ni
         ir = Ni + 1 - i
         lr3 = ein"(((((gea,abc),cd),ehfbpq),ghi),ij),dfj -> pq"(FL[i,j],ALu[i,j],Cu[i,j],ap[i,j],ALd[ir,j],Cd[ir,j],FR[i,j])
-        Mx = ein"pq, pq -> "(Array(lr3),σx)
-        My = ein"pq, pq -> "(Array(lr3),σy)
-        Mz = ein"pq, pq -> "(Array(lr3),σz)
+        Mx = ein"pq, pq -> "(Array(lr3),σx/2)
+        My = ein"pq, pq -> "(Array(lr3),σy/2)
+        Mz = ein"pq, pq -> "(Array(lr3),σz/2)
         n3 = ein"pp -> "(lr3)
         M[i,j] = ((Array(Mx)[]/Array(n3)[])^2+(Array(My)[]/Array(n3)[])^2+(Array(Mz)[]/Array(n3)[])^2)^0.5
     end
@@ -41,10 +47,10 @@ function magnetisation(degree, folder, D, χ, tol, maxiter, miniter)
 end
 
 Random.seed!(100)
-folder, D, χ, tol, maxiter, miniter = "/home/xyzhang/research/ADBCVUMPS.jl/data/all/", 3, 20, 1e-10, 10, 5
+folder, D, χ, tol, maxiter, miniter = "/home/xyzhang/research/ADBCVUMPS.jl/data/all/", 4, 20, 1e-10, 10, 2
 
 magplot = plot()
-degree = 85.0:5.0:95.0
+degree = [266.0:2.0:268.0; 272.0:2.0:274.0]
 mag, ferro, stripy, zigzag, Neel = [], [], [], [], []
 for x in degree
     y1, y2, y3, y4, y5 = magnetisation(x, folder, D, χ, tol, maxiter, miniter)
